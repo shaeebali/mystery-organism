3fa1c9f1fdeb0906374d9e316b7738c1a0f41672
shaeeb <shaeebali@gmail.com>
2023-02-06 11:04:50 +0000

    add remaining blocks of code and completed object

--- main.js
+++ main.js
@@ -13,7 +13,51 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// console.log(mockUpStrand())
 
+// Create a factory function to generate objects
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum,
+    dna,
+    mutate() {
+      let newBase = returnRandBase();
+      let randomBase = Math.floor(Math.random() * dna.length)
+      while (this.dna[randomBase] === newBase) {
+        newBase = returnRandBase();
+      }
+      this.dna = newBase;
+    },
+    compareDNA(pAequor) {
+      let identicalBaseCount = 0;
+      (this.dna).forEach(base, i => {
+        if (base === pAequor[i]) {
+          identicalBaseCount ++;
+        }
+      });
+      return Math.floor((identicalBaseCount / (this.dna).length.toFixed(2) * 100))
+    },
+    willLikelySurvive() {
+      let chanceOfSurvival = (this.dna).filter(base => base === 'C' || 'G')
+      if (((chanceOfSurvival.length / (this.dna).length) * 100) > 59) {
+        return true;
+      }
+      return false;
+    }
+  }
+}
+
+let sample = [];
+let i = 0;
+while (sample.length < 30) {
+  let temp = pAequorFactory(i, mockUpStrand());
+  if (temp.willLikelySurvive() == true) {
+    sample.push(temp);
+    i += 1
+  } 
+}
+pAequorFactory(1, mockUpStrand);
+console.log(pAequorFactory(1, mockUpStrand()))
 
 
 
